c1ecde55321ae75a4696124e650b3a25e24203c3
Hiroaki Mizuguchi <hiroaki-m@iij.ad.jp>
2021-05-13 13:51:04 +0900

    rewrite test

--- test.py
+++ test.py
@@ -1,19 +1,46 @@
+import io
+import textwrap
 import unittest
 from logging.config import fileConfig
 
 
 class TestLoggingConfig(unittest.TestCase):
+    config = textwrap.dedent(
+        """
+        [loggers]
+        keys=root
+        [logger_root]
+        level=INFO
+        handlers=console
+        [handlers]
+        keys=console
+        [handler_console]
+        class=logging.StreamHandler
+        formatter=console
+        args=(sys.stdout,)
+        [formatters]
+        keys=console
+        [formatter_console]
+        format=CONSOLE: %(asctime)s %(levelname)s %(message)s
+        class=%CLASS%
+        """
+    )
+
+    def file_config(self, cls, **kwargs):
+        f = io.StringIO(self.config.replace("%CLASS%", cls))
+        fileConfig(f, **kwargs)
+
     def test_case0(self):
-        fileConfig("case0.ini")
+        self.file_config("logging.Formatter")
 
     def test_case1(self):
-        fileConfig("case1.ini")
+        self.file_config("foo.logging.FooBarFormatter")
 
     def test_case2(self):
-        fileConfig("case2.ini")
+        self.file_config("bar.logging.FooBarFormatter")
 
     def test_case3(self):
-        fileConfig("case3.ini")
+        self.file_config("baz.logging.FooBarFormatter")
 
 
 if __name__ == "__main__":
